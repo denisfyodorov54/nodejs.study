617e010e8797d51d59881c32b8d24b625957fbad
denisfyodorov54 <denis.fyodorov54@gmail.com>
2024-11-14 00:57:41 +0100

    Task 9
    Endpoint with query string params were implemented

--- services/fetchData.js
+++ services/fetchData.js
@@ -1,10 +1,15 @@
 import getData from "../repository/nasaRepository.js";
-import mapData from "../helper/dataMapper.js";
+import { mapData, filterNearEarthObjects } from "../helper/dataMapper.js";
 
-const fetchData = async () => {
-  const NearEarthObjects = await getData();
-  const result = await mapData(NearEarthObjects);
-  return result;
+const fetchData = async (date, count, wereDangerousMeteors) => {
+  const nearEarthObjects = await getData(date);
+  const result = await mapData(nearEarthObjects);
+  const filteredNearEarthObjects = await filterNearEarthObjects(
+    result,
+    count,
+    wereDangerousMeteors
+  );
+  return filteredNearEarthObjects;
 };
 
 export default fetchData;
